f06a442fec74f73e269f12e4e77cb5501213a29c
Brent050830 <1787190669@qq.com>
2025-07-01 18:26:55 +0800

    Update katex.js

--- docs/javascripts/katex.js
+++ docs/javascripts/katex.js
@@ -9,24 +9,3 @@ document$.subscribe(({ body }) => {
   })
 })
 
-window.MathJax = {
-  tex: {
-    inlineMath: [["\\(", "\\)"]],
-    displayMath: [["\\[", "\\]"]],
-    processEscapes: true,
-    processEnvironments: true
-  },
-  options: {
-    ignoreHtmlClass: ".*|",
-    processHtmlClass: "arithmatex"
-  }
-};
-
-document$.subscribe(() => { 
-
-
-  MathJax.startup.output.clearCache()
-  MathJax.typesetClear()
-  MathJax.texReset()
-  MathJax.typesetPromise()
-})
