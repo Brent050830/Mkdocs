c4cd0efe1f6485ef54b6a867013e56ac8af19e87
Brent050830 <1787190669@qq.com>
2025-07-01 17:26:14 +0800

    1

--- docs/javascripts/katex.js
+++ docs/javascripts/katex.js
@@ -8,3 +8,25 @@ document$.subscribe(({ body }) => {
     ],
   })
 })
+
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => { 
+
+
+  MathJax.startup.output.clearCache()
+  MathJax.typesetClear()
+  MathJax.texReset()
+  MathJax.typesetPromise()
+})
